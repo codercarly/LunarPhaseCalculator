a12dbeb0e7cf79b1a6d1f4796bf4814b8438f756
codercarly <codercarly@gmail.com>
2018-11-21 12:57:21 -0500

    First commit

---
+++ eclipse-workspace/HelloWorld_Java/src/HelloWorld_Java.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld_Java {
+
+	public static void main(String[] args) {
+		
+		// Adding a comment to help
+		System.out.println("Hello World, this is Java");
+		
+		System.out.println("Adding something else.");
+
+	}
+
+}
